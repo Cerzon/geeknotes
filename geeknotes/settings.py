662bed1191f4b4d5fd259a74952849f2f8de5ccf
Cerzon <kirill.tkachenko@gmail.com>
2021-08-27 02:04:55 +0300

    lesson 02 homework - create react frontend application - add UserList component - add Menu component - add Footer component - add corsheaders to INSTALLED_APPS - add corsheaders middleware to MIDDLEWARE - add CORS_ALLOWED_ORIGINS setting - update requirements.txt

--- geeknotes/settings.py
+++ geeknotes/settings.py
@@ -47,12 +47,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'usersapp.apps.UsersappConfig',
+    'corsheaders',
+    'usersapp',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -142,3 +144,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Cors politics
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
+]
