b66bbabc8aa35fe5756a1638c7153da1248f1e8c
Cerzon <kirill.tkachenko@gmail.com>
2021-09-01 23:19:47 +0300

    add Project and Note pagination classes

--- notesapp/views.py
+++ notesapp/views.py
@@ -1,14 +1,25 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Project, Note
 from .serializers import ProjectModelSerializer, NoteModelSerializer
 
 
+class ProjectPageNumberPagination(PageNumberPagination):
+    page_size = 10
+
+
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    pagination_class = ProjectPageNumberPagination
+
+
+class NotePageNumberPagination(PageNumberPagination):
+    page_size = 20
 
 
 class NoteModelViewSet(ModelViewSet):
     queryset = Note.objects.all()
     serializer_class = NoteModelSerializer
+    pagination_class = NotePageNumberPagination
